7cb2f7d869db0d7240472370aaa12c6c1b3f0ecf
Vichukano <vichukano@gmail.com>
2022-02-11 09:58:33 +0300

    add Path examples

---
+++ src/main/java/ru/vichukano/ocp/ionio/NIOExamples.java
@@ -0,0 +1,27 @@
+package ru.vichukano.ocp.ionio;
+
+import java.nio.file.Path;
+
+/**
+ * resolve - склеить два пути. Первый путь может быть абсолютным и относительным.
+ * Если второй путь абсолютный, то вернет его. Если относительный, то склеит
+ * <p>
+ * relativize - как добраться из одной директории в другую
+ * Оба пути должны быть или абсолютными, или относительными, иначе IllegalArgumentException
+ */
+public class NIOExamples {
+
+    public static void main(String[] args) {
+        System.out.println("---------resolve---------");
+        Path current = Path.of("C:\\dir1\\dir2\\..\\dir3");
+        Path given = Path.of("dir4\\dir5");
+        Path abs = Path.of("D:\\dir6");
+        System.out.println("absolute + relative:\t" + current.resolve(given));
+        System.out.println("absolute + absolute returns param as is:\t" + current.resolve(abs));
+        System.out.println("---------relativize---------");
+        Path iAmHere = Path.of("/home/dir1/dir2");
+        Path wantToGetThere = Path.of("/dir3/dir4");
+        System.out.println("Path from iAmHere to wantToGetThere:\t" + iAmHere.relativize(wantToGetThere));
+    }
+
+}
